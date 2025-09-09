62be7967b0963c473d2aa134c391acae1121f69d
AungKhantKyaw2 <japangatone2020@gmail.com>
2025-09-09 13:25:35 +0700

    updated

--- l34webpackexercisewithform/dist/components/formhandler.js
+++
@@ -1,9 +0,0 @@
-export const formData = (form) => {
-    const inputs = form.querySelectorAll("input");
-    const values = {};
-    inputs.forEach((input) => {
-        const key = input.name || input.id;
-        values[key] = input.value;
-    });
-    return values;
-};
